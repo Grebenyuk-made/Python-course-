fab7bbf26ad90a41028e8e4adc96fbd3e41e19c7
Grenenyuk Dmitriy <sudo.grebenyuk.da@gmail.com>
2022-07-11 04:51:24 +0800

    tasks done 07072022DZ

---
+++ 07072022DZ/task2.py
@@ -0,0 +1,10 @@
+#Напишите программу для. проверки истинности 
+#утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = int(input('Введите значение X = '))
+y = int(input('Введите значение Y = '))
+z = int(input('Введите значение Z = '))
+if not (x or y or z) == (not x) and (not y) and (not z):
+    print(True)
+else:
+    print(False)
