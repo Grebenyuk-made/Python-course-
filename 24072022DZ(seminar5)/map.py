3350f0f25ff10c09f131351628ee09ff716fb3f4
Dmitriy Grebenyuk <107705705+Grebenyuk-made@users.noreply.github.com>
2022-08-25 02:07:45 +0800

    Delete 24072022DZ(seminar5) directory

--- 24072022DZ(seminar5)/map.py
+++
@@ -1,9 +0,0 @@
-#Примеры применения и использования Map.
-
-def sorting (number):
-    if number % 2 == 0:
-        return number
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-result = list(map(sorting, numbers))
-print(result)
--- 24072022DZ(seminar5)/zip.py
+++
@@ -1,9 +0,0 @@
-#Примеры применения и использования Zip.
-
-boys = ['Peter', 'Alex', 'John', 'Arthur', 'Richard']
-girls = ['Kate', 'Liza', 'Kira', 'Emma', 'Trisha']
-girls.sort()
-boys.sort()
-dating = zip(boys, girls)
-for company in dating:
-  print(f"{company[0]} и {company[1]}")
