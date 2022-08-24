6bb2d039e1bc6176d80aaf9c02e88998131699f6
Grebenyuk Dmitriy <sudo.grebenyu.da@gmail.com>
2022-08-25 01:58:02 +0800

    Добавил выполненое ДЗ за 5 семинар.

---
+++ 24072022DZ(seminar5)/lambda.py
@@ -0,0 +1,18 @@
+#Примеры применения и использования Lambda.
+
+def calс (pr , a , b):
+    print(pr(a  , b))
+
+mult = lambda x, y: x*y
+
+a1 = 25 
+b1 = 100
+calс(mult, a1,b1)
+
+a2 = 50 
+b2 = 30
+calс(mult, a2,b2)
+
+a3 = 17 
+b3 = 7
+calс(mult, a3,b3)
